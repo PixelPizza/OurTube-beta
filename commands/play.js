4d96b3ce61764a70a179d645a63f763f4c7fc0fb
jaronetje <60853956+jaronetje@users.noreply.github.com>
2020-05-27 12:47:19 +0200

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -1,7 +1,19 @@
 const {} = require('discord.js');
 const ytdl = require('ytdl-core-discord');
+const {getInfo} = require('ytdl-getinfo');
 
 module.exports = {
     name: "play",
-    description: "add music to the current queue"
+    description: "add music to the current queue",
+    aliases: ['p'],
+    agrs: true,
+    usage: "<search query>",
+    guildOnly: true,
+    async execute(message, args, client){
+        const connection = message.member.voice.channel.join();
+        const dispatcher = connection.play(await ytdl(args.join(" ")), {type: "opus"});
+        dispatcher.on('finish', () => {
+            connection.disconnect();
+        });
+    }
 }
