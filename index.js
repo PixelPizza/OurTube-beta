a9b510637d4c1fb8f430c30206fc5387cc19cbcd
jaronetje <60853956+jaronetje@users.noreply.github.com>
2020-05-27 22:18:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -22,8 +22,6 @@ client.on('ready', () => {
 });
 
 client.on('message', async message => {
-    console.log(client.queue);
-    
     if (!message.content.startsWith(prefix) || message.author.bot || message.webhookID || message.channel.id !== channels.music) return;
 
     const args = message.content.slice(prefix.length).split(/ +/);
@@ -95,6 +93,8 @@ client.on('message', async message => {
     try {
         await command.execute(message, args, client);
         const clientMember = message.guild.members.cache.get(client.user.id);
+        console.log(client.dispatcher);
+        console.log(clientMember.voice.channel);
         if (!client.dispatcher && clientMember.voice.channel){
             playSong();
         }
