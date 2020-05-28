d595cd5b708d8f8660315025b64b706bdd6a3fd7
jaronetje <60853956+jaronetje@users.noreply.github.com>
2020-05-28 22:35:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ client.on('message', async message => {
                 }, 1000);
                 return;
             }
-            client.dispatcher = client.connection.play(await ytdl(client.queue[0], {quality: 'highestaudio', filter: 'audioonly', highWaterMark: 2000}), {type: 'opus', volume: client.volume / 100, seek: 0});
+            client.dispatcher = client.connection.play(await ytdl(client.queue[0], {quality: 'highestaudio', filter: 'audioonly', highWaterMark: 2000}), {volume: client.volume / 100, seek: 10});
             client.seek = 0;
             client.dispatcher.on('finish', () => {
                 if (!client.loop || command.name === "skip"){
@@ -101,6 +101,7 @@ client.on('message', async message => {
                     playSong();
                 }, 1000);
             });
+            client.dispatcher.on('error', console.error);
         } else {
             client.dispatcher = null;
         }
