c165eb3541bdc66af4be9baf3c274538f2627b4a
jaronetje <60853956+jaronetje@users.noreply.github.com>
2020-05-31 21:19:54 +0200

    Update queue.js

--- commands/queue.js
+++ commands/queue.js
@@ -29,10 +29,38 @@ module.exports = {
             return message.channel.send(embedMsg);
         }
 
+        let count = 0;
         client.queue.forEach(async videoId => {
             let result = await getInfo(videoId);
             result = result.items[0];
-            console.log(result);
+            let hours = Math.floor(result.duration / 3600);
+            let seconds = result.duration % 3600;
+            let minutes = Math.floor(seconds / 60);
+            seconds = seconds % 60;
+            if (seconds < 10){
+                seconds = `0${seconds}`;
+            }
+            if (minutes < 10){
+                minutes = `0${minutes}`;
+            }
+            let duration = `${minutes}:${seconds}`;
+            if (hours > 0){
+                if (hours < 10){
+                    hours = `0${hours}`;
+                }
+                duration = `${hours}:${duration}`;
+            }
+            let video = `${result.creator} | [${result.fulltitle}](https://www.youtube.com/watch?v=${videoId}) | \`${duration}\``;
+            if (!embedMsg.fields.length){
+                embedMsg.addField("__Now Playing__", video);
+            } else if (embedMsg.fields.length == 1) {
+                embedMsg.addField("__Up Next__", `${count}. ${video}`);
+            } else {
+                embedMsg.fields[1].value = `${embedMsg.fields[1].value}\n\n${count}. ${video}`;
+            }
+            count++;
         });
+
+        message.channel.send(embedMsg);
     }
 }
