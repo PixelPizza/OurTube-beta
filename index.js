235e17b7e51ec3b6b9b157caaa5e122486f363df
jaronetje <60853956+jaronetje@users.noreply.github.com>
2020-05-31 16:00:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -59,14 +59,16 @@ client.on('message', async message => {
         client.volume = 50;
     }
 
-    if (!message.member.voice.channel || message.member.voice.channel.id !== voiceChannels.music){
-        embedMsg
-            .setColor(red)
-            .setTitle("Error 404")
-            .setDescription(`Voice channel not found`)
-            .setThumbnail("https://cdn.shopify.com/s/files/1/1061/1924/products/Anguished_Face_Emoji_1024x1024.png");
+    if (command.name != "prefix"){
+        if (!message.member.voice.channel || message.member.voice.channel.id !== voiceChannels.music){
+            embedMsg
+                .setColor(red)
+                .setTitle("Error 404")
+                .setDescription(`Voice channel not found`)
+                .setThumbnail("https://cdn.shopify.com/s/files/1/1061/1924/products/Anguished_Face_Emoji_1024x1024.png");
 
-        return message.channel.send(embedMsg);
+            return message.channel.send(embedMsg);
+        }
     }
 
     if (command.guildOnly && message.channel.type !== 'text') {
