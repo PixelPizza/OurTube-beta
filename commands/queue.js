4ce30f74c1ab93d911737097136fbd46f236996b
jaronetje <60853956+jaronetje@users.noreply.github.com>
2020-05-31 22:04:29 +0200

    Update queue.js

--- commands/queue.js
+++ commands/queue.js
@@ -30,7 +30,7 @@ module.exports = {
         }
 
         let count = 0;
-        for(let videoId in client.queue){
+        for(let videoId of client.queue){
             let result = await getInfo(videoId);
             result = result.items[0];
             let hours = Math.floor(result.duration / 3600);
@@ -62,6 +62,6 @@ module.exports = {
             if (count == client.queue.length){
                 message.channel.send(embedMsg);
             }
-        });
+        }
     }
 }
