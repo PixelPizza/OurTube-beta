abb771f866c2b461d786b0da45bbb710753f6e67
jaronetje <60853956+jaronetje@users.noreply.github.com>
2020-05-31 20:23:32 +0200

    Create queue.js

---
+++ commands/queue.js
@@ -0,0 +1,37 @@
+const {MessageEmbed} = require('discord.js');
+const {getInfo} = require('ytdl-getinfo');
+const {blue, red} = require('../colors.json');
+
+module.exports = {
+    name: "queue",
+    description: "show the queue",
+    aliases: ['q'],
+    args: false,
+    guildOnly: true,
+    async execute(message, args, client){
+        const embedMsg = new MessageEmbed()
+            .setColor(blue)
+            .setTitle("Queue");
+
+        if (!client.connection){
+            embedMsg
+                .setColor(red)
+                .setDescription(`I'm not connected to a voice channel!`);
+
+            return message.channel.send(embedMsg);
+        }
+
+        if (!client.dispatcher || !client.queue.length){
+            embedMsg
+                .setColor(red)
+                .setDescription(`I'm not playing anything!`);
+
+            return message.channel.send(embedMsg);
+        }
+
+        client.queue.forEach(async videoId => {
+            let result = await getInfo(videoId);
+            console.log(result);
+        });
+    }
+}
