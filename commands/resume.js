de1d8eff89e9d6f13776bb6f56f307025e733a52
mcpsbot <83839790+mcpsbot@users.noreply.github.com>
2021-05-30 17:17:36 +0100

    Resolved Issue #2

--- commands/resume.js
+++ commands/resume.js
@@ -7,7 +7,7 @@ module.exports = {
     aliases: ['res'],
     args: false,
     guildOnly: true,
-    execute(message, args, client)
+    execute(message, args, client){
 
         const embedMsg = new MessageEmbed()
             .setColor(blue)
