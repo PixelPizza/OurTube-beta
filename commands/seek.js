b9f5edc8bced8f45e733cd48f6444f4271c38d5c
mcpsbot <83839790+mcpsbot@users.noreply.github.com>
2021-06-05 19:16:38 +0100

    Update seek.js

--- commands/seek.js
+++ commands/seek.js
@@ -3,20 +3,14 @@ const  {blue} = require('../colors.json');
 const  {muscialEmojis} = require('../config.json');
 
 module.exports = {
-    name: "seek",
-    description: "let the bot join a voice channel",
+    name: "Seek",
+    description: "Choose a timestamp for the song to skip to",
     aliases: ['s'],
     args: false,
     guildOnly: true,
-    async execute(message, args, client){
-        const embedMsg = new MessageEmbed()
-            .setColor(blue)
-            .setTitle(`${muscialEmojis[Math.floor(Math.random() * muscialEmojis.length)]} Join ${muscialEmojis[Math.floor(Math.random() * muscialEmojis.length)]}`)
-            .setDescription(`Joined \`${message.member.voice.channel.name}\``);
+    
+client.on('message', async (message) => { const args = message.content.slice(settings.prefix.length).trim().split(/ +/g); c
+const command = args.shift().toLowerCase(); if(command === 'seek'){ // If provided 10 seconds, it would send the Milliseconds stamp (10 * 1000) let song = await client.player.seek(message, parseInt(message.args[0] * 1000)).catch(err => { return message.channel.send(error.message); }); message.channel.send(Seeked to ${message.args[0]} second of ${song.name}.); } });
 
-        if (!client.connection){
-            client.connection = await message.member.voice.channel.join();
-        }
-        message.channel.send(embedMsg);
-    }
-}
+        
+
