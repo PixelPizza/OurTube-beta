5a77f390cba47072c37123d4e6c0a68b462ce4f8
mcpsbot <83839790+mcpsbot@users.noreply.github.com>
2021-07-02 06:38:37 +0100

    Create a Invite Link for The bot

---
+++ commands/Invite.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: "invite",
+  code: `$title[Music Bot invite links]
+  $description[
+   Recommended: [Click here]($getBotInvite)
+   Admin: [Click here](https://discord.com/api/oauth2/authorize?client_id=BOT_ID&permissions=8&scope=bot)
+  $footer[Thanks for Choosing Me]`
+}
+© 2021 GitHub, Inc.
