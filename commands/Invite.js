aa53ddad970ebcccb25794083b352ddb8bf02e59
Jaron <60853956+JaronZ@users.noreply.github.com>
2021-07-03 19:20:00 +0200

    Fix: invite command (#12)
    * Fix: add invite imports
    
    * Fix: add right permissions for recommended
    
    * Feat: use object for deprecation problem

--- commands/Invite.js
+++ commands/Invite.js
@@ -1,14 +1,31 @@
+const {MessageEmbed, Client, Message} = require("discord.js"),
+  {stripIndents} = require("common-tags");
+
 module.exports = {
   name: "invite",
-  execute(message, args, client) {
+  /**
+   * Execute the invite command
+   * @param {Message} message The message that was sent
+   * @param {string[]} args The arguments that were used for this command
+   * @param {Client} client The client used to execute the command
+   */
+  async execute(message, args, client) {
     message.channel.send(new MessageEmbed({
       title: "Music Bot invite links",
       description: stripIndents`
-        Recommended: [Click here](${client.generateInvite(YOUR_PERMISSIONS_HERE)})
-        Admin: [Click here](${client.generateInvite(Permissions.FLAGS.ADMINISTRATOR)})
+        Recommended: [Click here](${await client.generateInvite({
+          permissions: [
+            "SEND_MESSAGES",
+            "EMBED_LINKS",
+            "MANAGE_MESSAGES",
+            "MANAGE_GUILD"
+          ]
+        })})
+        Admin: [Click here](${await client.generateInvite({
+          permissions: ["ADMINISTRATOR"]
+        })})
       `,
       footer: "Thanks for choosing me"
     }));
   }
 }
-
