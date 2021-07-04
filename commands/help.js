01c46f7b6ab6d6c454716fddda1e9187abb79751
mcpsbot <83839790+mcpsbot@users.noreply.github.com>
2021-07-04 16:59:59 +0000

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -8,6 +8,7 @@ module.exports = {
 	usage: '[command name]',
 	cooldown: 5,
 	execute(message, args) {
-		// ...
+		const data  = [];                                                                                                     
+		const  { commands } = message.client;  
 	}
 }
