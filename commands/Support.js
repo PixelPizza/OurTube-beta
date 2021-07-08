858658b0df960b64701ea655bd71a9e180b62400
mcpsbot <83839790+mcpsbot@users.noreply.github.com>
2021-07-08 12:03:36 +0100

    Update Support.js

--- commands/Support.js
+++ commands/Support.js
@@ -1,1 +1,12 @@
+module.exports = {
+  name: "support",
+  description: "get the invite link of the support server",
+  execute(message, args){
+    message.channel.send(new MessageEmbed({
+      color: "BLUE",
+      title: "Support",
+      description: "Here is the invite for the [support server](https://discord.gg/xf2zskzg48)"
+    }));
+  }
+}
 
